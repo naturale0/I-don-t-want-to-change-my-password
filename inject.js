34fa27953ef19ac6cdf13b4debf6ff0b88625f60
naturale0 <naturale0@snu.ac.kr>
2021-08-26 16:10:03 +0900

    initial commit

---
+++ inject.js
@@ -0,0 +1,9 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+	// just place a div at top right
+	//goMySnu();
+	document.getElementsByClassName("BT")[2].click();
+
+})();

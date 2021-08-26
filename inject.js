278abe61f455d9c87505fefb038a0d1c2cc28a65
naturale0 <naturale0@snu.ac.kr>
2021-08-26 16:49:47 +0900

    bump up version

--- inject.js
+++ inject.js
@@ -1,6 +1,9 @@
 // the code which will be injected
 
 (function() {
-	window.location.replace('https://etl.snu.ac.kr');
+	var url = window.location.toString();
+	url = url.replace("https://idm.snu.ac.kr/imc_change_pwd_form_mysnu.jsp?si_redirect_address=", "");
+	url = url.replace("http://idm.snu.ac.kr/imc_change_pwd_form_mysnu.jsp?si_redirect_address=", "");
+	window.location.replace(url);
 	//document.getElementsByClassName("BT")[2].click();
 })();
